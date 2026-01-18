20d37cd62e5d2938f55d71f69a9cbe660c63912e
Warren Jitsing <warren.jitsing@infcon.co.za>
2026-01-18 14:39:53 +0000

    :star: new(context): add GatewayContext

--- src/context.rs
+++ src/context.rs
@@ -20,3 +20,30 @@
 //!         * Calculate `TypeId::of::<T>()`.
 //!         * Look up the box in the map.
 //!         * Use `downcast_ref` to cast `dyn Any` back to `&T`.
+use std::any::{Any, TypeId};
+use std::collections::HashMap;
+
+pub struct GatewayContext {
+    state: HashMap<TypeId, Box<dyn Any + Send + Sync>>,
+}
+
+impl GatewayContext {
+    pub fn new() -> Self {
+        GatewayContext {
+            state: HashMap::new(),
+        }
+    }
+
+    pub fn insert<T: Send + Sync + 'static>(&mut self, val: T) -> Option<T> {
+        let key = TypeId::of::<T>();
+        let old_boxed = self.state.insert(key, Box::new(val));
+        old_boxed.map(|boxed| {
+            *boxed.downcast::<T>().expect("GatewayContext: TypeId mismatch")
+        })
+    }
+
+    pub fn get<T: 'static>(&self) -> Option<&T> {
+        let key = TypeId::of::<T>();
+        self.state.get(&key).and_then(|boxed| boxed.downcast_ref())
+    }
+}
