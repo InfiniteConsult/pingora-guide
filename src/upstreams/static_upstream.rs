ac9f4fcea549c9ddf4600e57aa3c3fb44202b741
Warren Jitsing <warren.jitsing@infcon.co.za>
2026-01-19 07:43:14 +0000

    :zap: improvement(static_upstream): add certificate key for mTLS

--- src/upstreams/static_upstream.rs
+++ src/upstreams/static_upstream.rs
@@ -16,9 +16,11 @@
 //!         * Construct a `Box<HttpPeer>` using the stored configuration.
 //!         * This is a "dumb" connector; it does no load balancing or health checking.
 //!         * Useful for admin APIs or simple sidecars.
+use std::sync::Arc;
 use async_trait::async_trait;
 use pingora::prelude::Session;
 use pingora::upstreams::peer::HttpPeer;
+use pingora::utils::tls::CertKey;
 use crate::context::GatewayContext;
 use crate::upstream::Upstream;
 use crate::error::Result;
@@ -28,22 +30,28 @@ pub struct StaticUpstream {
     addr: (String, u16),
     tls: bool,
     sni: String,
+    client_cert: Option<Arc<CertKey>>
 }
 
 impl StaticUpstream {
-    pub fn new(addr: (String, u16), tls: bool, sni: String) -> Self {
-        StaticUpstream { addr, tls, sni }
+    pub fn new(addr: (String, u16), tls: bool, sni: String, client_cert: Option<Arc<CertKey>>) -> Self {
+        StaticUpstream { addr, tls, sni, client_cert }
     }
 }
 
 #[async_trait]
 impl Upstream for StaticUpstream {
     async fn select_peer(&self, _session: &mut Session, _ctx: &mut GatewayContext) -> Result<Box<HttpPeer>> {
-        Ok(Box::new(HttpPeer::new(
+        let mut peer = HttpPeer::new(
             self.addr.clone(),
             self.tls,
             self.sni.clone(),
-        )))
+        );
+
+        if let Some(cert) = &self.client_cert {
+            peer.client_cert_key = Some(cert.clone());
+        }
+        Ok(Box::new(peer))
     }
 }
 
