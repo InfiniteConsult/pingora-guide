552324e036c52a5c5e2c6029ac90886606e6ba79
Warren Jitsing <warren.jitsing@infcon.co.za>
2026-01-22 10:16:44 +0000

    :star: new(router): add Router struct and implementation logic

--- src/upstreams/router.rs
+++ src/upstreams/router.rs
@@ -18,3 +18,86 @@
 //!         * If found: Call `select_peer()` on the matched child upstream.
 //!         * If not found: Return `Err(Error::Gateway(GatewayError::NoRouteMatched))`
 //!           (which typically translates to a 404).
+use std::collections::HashMap;
+use regex::Regex;
+use crate::config::{RouteConf, GatewayConf, PathType};
+
+#[derive(Debug)]
+pub struct Router {
+    pub regex_routes: Vec<(Regex, usize)>,
+    pub matchit_router: matchit::Router<usize>,
+    pub route_registry: HashMap<usize, RouteConf>
+}
+
+impl Router {
+    pub fn new(conf: &GatewayConf) -> Self {
+        let mut regex_routes: Vec<(Regex, usize)> = Vec::new();
+        let mut matchit_router: matchit::Router<usize> = matchit::Router::new();
+        let mut route_registry: HashMap<usize, RouteConf> = HashMap::new();
+
+        for (idx, route) in conf.routes.iter().enumerate() {
+            route_registry.insert(idx, route.clone());
+            match route.path_type {
+                PathType::Regex => {
+                    let re = Regex::new(&route.path).expect("Invalid Regex in configuration");
+                    regex_routes.push((re, idx));
+                },
+                PathType::Prefix | PathType::Exact => {
+                    if !route.path.starts_with('/') {
+                        eprintln!("Warning: Prefix route '{}' does not start with '/'", route.path);
+                        continue;
+                    }
+
+                    match matchit_router.insert(&route.path, idx) {
+                        Ok(_) => {},
+                        Err(e) => { eprintln!("Failed to insert route '{}': {}", route.path, e); }
+                    }
+                }
+            }
+        }
+
+        Self {
+            regex_routes,
+            matchit_router,
+            route_registry
+        }
+    }
+
+    pub fn match_request(&self, path: &str, host: Option<&str>) -> Option<&RouteConf> {
+        for (re, id) in &self.regex_routes {
+            if re.is_match(path) {
+                let route = &self.route_registry[id];
+                if self.validate_host(route, host) {
+                    return Some(route)
+                }
+            }
+        }
+
+        if let Ok(match_result)  = self.matchit_router.at(path) {
+            let id = match_result.value;
+            let route = &self.route_registry[id];
+
+            if matches!(route.path_type, PathType::Exact) && path != route.path {
+                return None;
+            }
+
+            if self.validate_host(route, host) {
+                return Some(route)
+            }
+        }
+
+        None
+    }
+
+    fn validate_host(&self, route: &RouteConf, host: Option<&str>) -> bool {
+        match &route.hostnames {
+            Some(allowed_hosts) => {
+                match host {
+                    Some(h) => allowed_hosts.iter().any(|allowed| allowed == h),
+                    None => false
+                }
+            },
+            None => true
+        }
+    }
+}
