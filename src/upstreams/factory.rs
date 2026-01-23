b6e29b0e51cf325866f9610f8f62df4e13eb1abe
Warren Jitsing <warren.jitsing@infcon.co.za>
2026-01-23 18:16:30 +0000

    :zap: improvement(factory): support YAML and JSON with file discovery

--- src/upstreams/factory.rs
+++ src/upstreams/factory.rs
@@ -20,7 +20,7 @@ use pingora::protocols::l4::socket::SocketAddr;
 use pingora::services::listening::Service;
 use pingora::services::background::BackgroundService;
 
-use crate::config::{UpstreamConf, UpstreamSource, LoadBalancerSelection, HealthCheckConf};
+use crate::config::{UpstreamConf, UpstreamSource, LoadBalancerSelection, HealthCheckConf, FileFormat, BackendConf};
 use crate::upstream::Upstream;
 use crate::upstreams::load_balancer::LoadBalancerCluster;
 use crate::upstreams::static_upstream::StaticUpstream;
@@ -54,37 +54,34 @@ impl ServiceDiscovery for DnsDiscovery {
 
 pub struct FileDiscovery {
     pub path: PathBuf,
+    pub format: FileFormat,
 }
 
 #[async_trait]
 impl ServiceDiscovery for FileDiscovery {
     async fn discover(&self) -> Result<(BTreeSet<Backend>, HashMap<u64, bool>)> {
-        let contents = match tokio::fs::read_to_string(&self.path).await {
-            Ok(c) => c,
-            Err(e) => {
-                error!("Failed to read upstream file: {}", e);
-                return Err(Error::explain(ErrorType::InternalError, e.to_string()))
-            },
+        let content = tokio::fs::read_to_string(&self.path).await
+            .map_err(|e| Error::explain(ErrorType::InternalError, e.to_string()))?;
+
+        let backend_confs: Vec<BackendConf> = match self.format {
+            FileFormat::Yaml => serde_yaml::from_str(&content)
+                .map_err(|e| Error::explain(ErrorType::InternalError, e.to_string()))?,
+            FileFormat::Json => serde_json::from_str(&content)
+                .map_err(|e| Error::explain(ErrorType::InternalError, e.to_string()))?,
         };
 
         let mut upstreams = BTreeSet::new();
-        for line in contents.lines() {
-            let line = line.trim();
-            if line.is_empty() || line.starts_with("#") {
-                continue;
-            }
-            match lookup_host(line).await {
-                Ok(mut addrs) => {
-                    if let Some(addr) = addrs.next() {
-                        let backend = Backend {
-                            addr: SocketAddr::Inet(addr),
-                            weight: 1,
-                            ext: Extensions::new()
-                        };
-                        upstreams.insert(backend);
-                    }
-                },
-                Err(e) => error!("Failed to parse address '{}': {}", line, e),
+        for b_conf in backend_confs {
+            let socket_addrs = b_conf.address.to_socket_addrs()
+                .map_err(|e| Error::explain(ErrorType::InternalError, e.to_string()))?;
+
+            for addr in socket_addrs {
+                let  backend = Backend {
+                    addr: SocketAddr::Inet(addr),
+                    weight: b_conf.weight as usize,
+                    ext: Extensions::new(),
+                };
+                upstreams.insert(backend);
             }
         }
         Ok((upstreams, HashMap::new()))
